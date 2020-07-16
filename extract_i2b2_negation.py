c4105e22f45d06c7d32e4bbb2a16e943da9512c9
Tim Miller <timothy.miller@childrens.harvard.edu>
2020-07-16 13:48:23 -0400

    Add whitelist of test files to use from non-leaky sources.

--- extract_i2b2_negation.py
+++ extract_i2b2_negation.py
@@ -13,6 +13,7 @@ class Concept:
         self.negated = negated
 
 concept_patt = re.compile('c="(.*)" (\d+):(\d+) (\d+):(\d+)')
+test_files = ['0002','0006','0010','0014','0018','0022','0026','0030','0034','0038','0042','0046','0050','0054','0058','0062','0066','0070','0074','0078','0082','0086','0090','0094','0098','0102','0106','0110','0114','0118','0122','0126','0130','0134','0138','0142','0146','0150','0154','0158','0162','0166','0170','0174','0178','0182','0186','0190','0194','0198','0202','0206','0210','0214','0218','0222','0226','0230','0234','0238','0242','0246','0250','0254','0258','0262','0266','0270','0274','0278','0282','0286','0290','0294','0298','0302','0306','0310','0314','0318','0322','0326','0330','0334','0338','0342','0346','0350','0354','0358','0362','0366','0370','0374','0378','0382','0386','0390','0394','0398','0402','0406','0410','0413','0416','0419','0422','0425','0428','0431','0434','0437','0440','0443','0446','0449','0452','0455','0458','0461','0463','0465','0467','0468','0469','0470','0471','0472','0473','0474','0475','0476','0477']
 
 def get_file_concepts(con_fn):
     concepts = []
@@ -86,14 +87,18 @@ def main(args):
 #             text_dir = join(test_note_dir, subdir, 'txt')
 
             for file in listdir(concept_dir):
+                record_name = file[:-4]
                 if not file.endswith('.ast'):
                     continue
 
+                if not record_name in test_files:
+                    continue
+                
                 # Create concept data structure for each concept annotation.
                 concepts = get_file_concepts(join(concept_dir, file))
 
                 # Now go through text and find the sentence context for each concept we found:
-                text_fn = join(test_note_dir, '%s.txt' % (file[:-4]))
+                text_fn = join(test_note_dir, '%s.txt' % (record_name))
                 text_lines = get_text_lines(text_fn)
                 
                 for concept in concepts:
