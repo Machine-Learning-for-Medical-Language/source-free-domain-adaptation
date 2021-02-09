b373535da1a05319920bdda6f4bce3e9b4be9e02
Tim Miller <timothy.miller@childrens.harvard.edu>
2021-02-09 10:11:28 -0500

    Added test set spans and labels, and updates to the script that processes unlabeled and labeled anafora files to create test set

--- extract_test_spans.py
+++ extract_test_spans.py
@@ -2,21 +2,33 @@
 
 import sys
 from anafora import walk, AnaforaData
-from os.path import join
+from os.path import join, exists
 
 def main(args):
-    if len(args) < 1:
-        sys.stderr.write('Required argument(s): <anafora directory>\n')
+    if len(args) < 2:
+        sys.stderr.write('Required argument(s): <unlabeled anafora directory> <labeled anafora directory>\n')
         sys.exit(-1)
 
-    for sub_dir, text_name, xml_names in walk(args[0], xml_name_regex='[.]dave\.completed\.xml$'):
+    for sub_dir, text_name, xml_names in walk(args[0], xml_name_regex='[.]dave\.inprogress\.xml$'):
         # print("text_name = %s" % text_name)
         assert len(xml_names) == 1
         xml_file = join(args[0], sub_dir, xml_names[0])
+        gold_file = join(args[1], sub_dir, xml_names[0].replace('inprogress', 'completed'))
+        use_gold = False
+
         # print("xml file = %s" % (xml_file,))
-        anafora_data = AnaforaData.from_file(xml_file)
+        if exists(gold_file):
+            anafora_data = AnaforaData.from_file(gold_file)
+            use_gold = True
+        else:
+            anafora_data = AnaforaData.from_file(xml_file)
+        
         for event in anafora_data.annotations.select_type('CuiEntity'):
-            negated = event.properties['negated']
+            if use_gold:
+                negated = event.properties['negated']
+            else:
+                negated = "Unlabeled"
+            
             span = event.spans
             assert len(span) == 1
             span = span[0]
